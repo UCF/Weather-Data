a1a90e0374a0f9643478ba4fa0e5917156dbc979
Jo Greybill <jogreybill@gmail.com>
2013-10-16 12:16:28 -0400

    Use nicer condition phrases; updated constants

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 define('SITE_URL', 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
 define('SITE_PATH', dirname(__FILE__).'/');
 
-define('CACHEDATA_PATH_CURRENT', SITE_PATH.'data/data-current.txt');
-define('CACHEDATA_PATH_FORECAST', '');
+define('CACHEDATA_PATH', SITE_PATH.'data/');
+define('CACHEDATA_CURRENT', CACHEDATA_PATH.'data-current.txt');
+define('CACHEDATA_FORECAST', '');
 
-define('WEATHER_URL_CURRENT', 'http://w1.weather.gov/xml/current_obs/KORL.xml');
+define('WEATHER_URL_CURRENT', 'http://w1.weather.gov/xml/current_obs/KORL.xml'); // URL to NOAA weather XML (current)
 define('WEATHER_URL_FORECAST', '');
 
 define('WEATHER_URL_TIMEOUT', 8);			// seconds
@@ -22,12 +23,12 @@ define('CACHE_DELIMITER', '!@!');
 function get_weather_data($forecasttype='current') {	
 	switch ($forecasttype) {
 		case 'forecast':
-			$cache_data 	= CACHEDATA_PATH_FORECAST;
+			$cache_data 	= CACHEDATA_FORECAST;
 			$weather_url 	= WEATHER_URL_FORECAST;
 			break;
 		case 'current':
 		default:
-			$cache_data 	= CACHEDATA_PATH_CURRENT;
+			$cache_data 	= CACHEDATA_CURRENT;
 			$weather_url 	= WEATHER_URL_CURRENT;
 			break;
 	}
@@ -104,7 +105,6 @@ function make_new_cachedata($cache_data, $weather_url) {
 		$xml = simplexml_load_string($raw_weather);
 		
 		// Make sure we get actual usable values before assigning them
-		$weather['condition'] 	= !empty($xml->weather) ? (string)$xml->weather : null;
 		$weather['temp']		= preg_match('/[0-9]+/', $xml->temp_f) ? number_format((string)$xml->temp_f).'&#186;' : null; // strip decimal place
 		$weather['imgCode']		= !empty($xml->icon_url_name) ? (string)$xml->icon_url_name : null;
 		
@@ -115,105 +115,139 @@ function make_new_cachedata($cache_data, $weather_url) {
 		switch ($weather_img_name) {
 			case 'bkn':
 				$weather_code = 28; // Mostly Cloudy
+				$weather_condition = 'Mostly cloudy';
 				break;
 			case 'nbkn':
 				$weather_code = 27; // Mostly Cloudy (night)
+				$weather_condition = 'Mostly cloudy';
 				break;
 			case 'skc':
 				$weather_code = 32; // Fair, Clear
+				$weather_condition = 'Fair';
 				break;
 			case 'nskc':
 				$weather_code = 31; // Fair, Clear (night)
+				$weather_condition = 'Fair';
 				break;
 			case 'few':
 				$weather_code = 34; // Few Clouds
+				$weather_condition = 'Fair';
 				break;
 			case 'nfew':
 				$weather_code = 29; // Few Clouds (night)
+				$weather_condition = 'Fair';
 				break;
 			case 'sct':
 				$weather_code = 30; // Partly Cloudy
+				$weather_condition = 'Partly cloudy';
 				break;
 			case 'nsct':
 				$weather_code = 27; // Partly Cloudy (night)
+				$weather_condition = 'Partly cloudy';
 				break;
 			case 'ovc':
 			case 'novc':
 				$weather_code = 26; // Overcast (day, night)
+				$weather_condition = 'Overcast';
 				break;
 			case 'fg':
 			case 'nfg':
 				$weather_code = 20; // Foggy
+				$weather_condition = 'Foggy';
 				break;
 			case 'smoke':
 				$weather_code = 22; // Smoke
+				$weather_condition = 'Smoke';
 				break;
 			case 'fzra':
 				$weather_code = 8;  // Freezing drizzle
+				$weather_condition = 'Freezing drizzle';
 				break;
 			case 'ip':
 				$weather_code = 18; // Hail
+				$weather_condition = 'Hail';
 				break;
 			case 'mix':
 			case 'nmix':
 				$weather_code = 7;  // Mixed snow and sleet (day, night)
+				$weather_condition = 'Mixed snow/sleet';
 				break;	
 			case 'raip':
 				$weather_code = 35; // Mixed rain and hail
+				$weather_condition = 'Mixed rain/hail';
 				break;	
 			case 'rasn':
 			case 'nrasn':
 				$weather_code = 6;  // Mixed rain and sleet
+				$weather_condition = 'Mixed rain/sleet';
 				break;	
 			case 'shra':
 				$weather_code = 11; // Light Showers
+				$weather_condition = 'Showers';
 				break;
 			case 'tsra':
 				$weather_code = 3;  // Severe Thunderstorms
+				$weather_condition = 'Severe thunderstorms';
 				break;	
 			case 'ntrsa':
 			case 'hi_ntrsa':
 				$weather_code = 47; // Thunderstorms, Thunderstorm in vicinity (night)
+				$weather_condition = 'Isolated thundershowers';
 				break;
 			case 'sn':
 				$weather_code = 16; // Snow
+				$weather_condition = 'Snow';
 				break;	
 			case 'nsn':
 				$weather_code = 46; // Snow (night)
+				$weather_condition = 'Snow';
 				break;
 			case 'wind':
 			case 'nwind':
+				$weather_code = 23; // Windy
+				$weather_condition = 'Windy';
+				break;
 			case 'nsvrtsra':
-				$weather_code = 23; // Windy (and funnel spout/tornado)
+				$weather_code = 0; // Funnel spout/tornado
+				$weather_condition = 'Tornado';
 				break;
 			case 'hi_shwrs':
 				$weather_code = 40; // Showers in Vicinity
+				$weather_condition = 'Scattered showers';
 				break;
 			case 'hi_nshwrs':
 			case 'nra':
 				$weather_code = 45; // Showers, Showers in Vicinity (night)
+				$weather_condition = 'Scattered showers';
 				break;
 			case 'fzrara':
 				$weather_code = 10; // Freezing Rain
+				$weather_condition = 'Freezing rain';
 				break;	
 			case 'hi_tsra':
 				$weather_code = 38; // Thunderstorm in vicinity (day)
+				$weather_condition = 'Scattered thunderstorms';
 				break;	
 			case 'ra1':
-				$weather_code = 9;  // Drizzle 
+				$weather_code = 9;  // Drizzle
+				$weather_condition = 'Drizzle';
 				break;
 			case 'ra':
-				$weather_code = 12; // Showers 
+				$weather_code = 12; // Showers
+				$weather_condition = 'Showers';
 				break;	
 			case 'dust':
 				$weather_code = 19; // Dust
+				$weather_condition = 'Dust';
 				break;
 			case 'mist':
 				$weather_code = 21; // Haze
+				$weather_condition = 'Haze';
 				break;					
 		}
 		$weather['imgCode'] = $weather_code;
-		
+		$weather['condition'] = $weather_condition;
+
 		// We assume the fetch was a success unless the
 		// condition, temp or imgCode are empty:
 		$weather['successfulCache'] = 'yes';
